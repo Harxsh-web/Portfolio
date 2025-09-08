de119e9d8d361419a94ec7fe55c7db28abb2e83c
Harxsh-web <hk3631311@gmail.com>
2025-09-08 23:41:37 +0530

    Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import { NavbarDemo } from "@/components/ui/NavbarDemo";
+import { HeroSection } from "./Home/HeroSection";
+
+
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-100">
+      {/* <NavbarDemo /> */}
+      <section className=" ">
+      <HeroSection/>        
+      </section>
+    </main>
+  );
+}
